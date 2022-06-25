fd07f2cfb8220fba10054893f2892e51c88ef0b4
Kidou07 <muhammadsabil345@gmail.com>
2022-06-25 10:19:32 +0700

    [awal]

--- controller/tugasController.js
+++ controller/tugasController.js
@@ -1,4 +1,8 @@
 const sequelize=require('sequelize');
 const tugasModel=require('../model/tugasModel')
 const bcrypt=require('bcrypt')
+const jadwalModel=require('../model/jadwalModel')
 
+class tugasController{
+    static
+}
